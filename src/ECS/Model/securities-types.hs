9f6ef0b3966248b366070ba5aacdc14532862141
Rafael Barros Felix de Sousa <rafaelbfs1@gmail.com>
2014-12-15 13:23:00 -0200

    Adding BDI Types

--- src/ECS/Model/securities-types.hs
+++ src/ECS/Model/securities-types.hs
@@ -6,3 +6,6 @@ module ECS.Model.SecuritiesTypes (
 
 
 data TradeType = Spot | OptBuyExc | OptSellExc | Auction | Fraction | Term |  FutureRetainedEarnings | FutureContinuousMov | OptBuy | OptSell
+data BDI = StandardIssue | Bankrupt | ReceiptsClaims | RealEstateFunds | Bonds | Obligations | PrivateMarketBonus | InsuranceBonusPublicBonds | IndexBuyOptions | IndexSellOptions | OptionBuyingExc | OptionSellingExc | UnquotedSecuritiesAuction | Auction | BustedStocksAuction | SalesBySapena | Others | TradeForStocks | Target | Term | BondsExpiryBefore3Years | BondsExpiryAfter3Years | BDIFutureCont | BDIFutureRetaindedEarnings | BondsAndPromises | FractionaryMarket | Total
+
+
